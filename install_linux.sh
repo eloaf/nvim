937ceb7f9b7245a33b25b88e262d2a8f7d139a96
Eric Laufer <eric@dreamfold.ai>
2024-01-19 12:14:38 -0500

    install script for linux

---
+++ install_linux.sh
@@ -0,0 +1,17 @@
+sudo apt -y update
+sudo apt install -y npm nodejs ack curl htop fzf fuse
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+
+sudo mv nvim.appimage /usr/local/bin/nvim.appimage
+
+echo "TERM=ansi-256color nvim.appimage" > nvim
+chmod u+x nvim
+sudo mv nvim /usr/local/bin/nvim
+
+rm -r ~/.config/nvim
+git clone https://github.com/eloaf/nvim.git ~/.config/nvim
+
+sudo npm install -g pyright
+pip install black isort
