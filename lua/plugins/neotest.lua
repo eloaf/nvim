ff13a779850c8568d2a2dedf5a64605fb09e5f85
Eric Laufer <eric@dreamfold.ai>
2024-03-19 07:48:01 -0700

    depency

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -4,7 +4,8 @@ return {
         dependencies = {
             "nvim-lua/plenary.nvim",
             "antoinemadec/FixCursorHold.nvim",
-            "nvim-treesitter/nvim-treesitter"
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-neotest/nvim-nio"
         },
     },
     {
