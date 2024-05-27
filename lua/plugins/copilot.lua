1d4f63c26f2f70f9b3c6c4ed0017d3f41cbfe648
Eric Laufer <eric@dreamfold.ai>
2024-05-27 15:17:24 -0400

    Copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -49,3 +49,24 @@ return {
         })
     end,
 }
+
+-- return {}
+
+-- return {
+--     'github/copilot.vim',
+--     -- config = false,
+--     init = function()
+--         vim.keymap.set('i', '<C-j>', 'copilot#Accept("<CR>")', {
+--             noremap = true,
+--             silent = true,
+--             expr = true,
+--             replace_keycodes = false
+--         })
+--
+--         vim.g.copilot_no_tab_map = true
+--         vim.g.copilot_assume_mapped = true
+--     end,
+--     config = function()
+--         vim.g.copilot_assume_mapped = true
+--     end
+-- }
--- lua/plugins/copilotchat.lua
+++ lua/plugins/copilotchat.lua
@@ -1,28 +1,62 @@
 -- https://github.com/jellydn/CopilotChat.nvim?tab=readme-ov-file#lazynvim
+-- return {
+--     {
+--         "jellydn/CopilotChat.nvim",
+--         opts = {
+--             mode = "split", -- newbuffer or split  , default: newbuffer
+--         },
+--         build = function()
+--             vim.defer_fn(function()
+--                 vim.cmd("UpdateRemotePlugins")
+--                 vim.notify("CopilotChat - Updated remote plugins. Please restart Neovim.")
+--             end, 3000)
+--         end,
+--         event = "VeryLazy",
+--         keys = {
+--             { "<leader>cce", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explain code" },
+--             { "<leader>cct", "<cmd>CopilotChatTests<cr>",   desc = "CopilotChat - Generate tests (with pytest)" },
+--         },
+--         -- config = function()
+--         -- TODO why doesnt this work
+--         -- local utils = require('CopilotChat.utils')
+--         -- utils.create_cmd('CopilotChatPytest', function()
+--         --     vim.cmd('CopilotChat ' ..
+--         --         'Briefly explain how the selected code works then generate unit tests using pytest.')
+--         -- end, { nargs = '*', range = true })
+--         -- end
+--     },
+-- }
+
+
 return {
     {
-        "jellydn/CopilotChat.nvim",
+        "CopilotC-Nvim/CopilotChat.nvim",
+        branch = "canary",
+        dependencies = {
+            { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+            { "nvim-lua/plenary.nvim" },  -- for curl, log wrapper
+        },
         opts = {
-            mode = "split", -- newbuffer or split  , default: newbuffer
+            debug = true, -- Enable debugging
+            -- See Configuration section for rest
         },
-        build = function()
-            vim.defer_fn(function()
-                vim.cmd("UpdateRemotePlugins")
-                vim.notify("CopilotChat - Updated remote plugins. Please restart Neovim.")
-            end, 3000)
+        -- See Commands section for default commands if you want to lazy load on them
+        config = function()
+            require("CopilotChat").setup {}
         end,
-        event = "VeryLazy",
-        keys = {
-            { "<leader>cce", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explain code" },
-            { "<leader>cct", "<cmd>CopilotChatTests<cr>",   desc = "CopilotChat - Generate tests (with pytest)" },
+        -- default window options
+        window = {
+            layout = 'vertical',    -- 'vertical', 'horizontal', 'float'
+            -- Options below only apply to floating windows
+            relative = 'editor',    -- 'editor', 'win', 'cursor', 'mouse'
+            border = 'single',      -- 'none', single', 'double', 'rounded', 'solid', 'shadow'
+            width = 0.8,            -- fractional width of parent
+            height = 0.6,           -- fractional height of parent
+            row = nil,              -- row position of the window, default is centered
+            col = nil,              -- column position of the window, default is centered
+            title = 'Copilot Chat', -- title of chat window
+            footer = nil,           -- footer of chat window
+            zindex = 1,             -- determines if window is on top or below other floating windows
         },
-        -- config = function()
-        -- TODO why doesnt this work
-        -- local utils = require('CopilotChat.utils')
-        -- utils.create_cmd('CopilotChatPytest', function()
-        --     vim.cmd('CopilotChat ' ..
-        --         'Briefly explain how the selected code works then generate unit tests using pytest.')
-        -- end, { nargs = '*', range = true })
-        -- end
     },
 }
