313a76e93d9b35683c495a6e421fb95f66b97efa
Eric Laufer <eric@dreamfold.ai>
2024-05-28 10:01:05 -0400

    add telescope extension to luasnip

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,25 +1,37 @@
 -- return {}
 -- config = function() require('config.snippets') end,
 return {
-    "L3MON4D3/LuaSnip",
-    -- follow latest release.
-    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-    -- install jsregexp (optional!).
-    build = "make install_jsregexp",
-    config = function()
-        local ls = require("luasnip")
-        require("luasnip.loaders.from_vscode").lazy_load()
-        vim.keymap.set({ "i" }, "<C-K>", function() ls.expand() end, { silent = true })
-        vim.keymap.set({ "i", "s" }, "<C-L>", function() ls.jump(1) end, { silent = true })
-        vim.keymap.set({ "i", "s" }, "<C-J>", function() ls.jump(-1) end, { silent = true })
+    {
+        "L3MON4D3/LuaSnip",
+        -- follow latest release.
+        version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- install jsregexp (optional!).
+        build = "make install_jsregexp",
+        config = function()
+            local ls = require("luasnip")
+            require("luasnip.loaders.from_vscode").lazy_load()
+            vim.keymap.set({ "i" }, "<C-K>", function() ls.expand() end, { silent = true })
+            vim.keymap.set({ "i", "s" }, "<C-L>", function() ls.jump(1) end, { silent = true })
+            vim.keymap.set({ "i", "s" }, "<C-J>", function() ls.jump(-1) end, { silent = true })
 
-        vim.keymap.set({ "i", "s" }, "<C-E>", function()
-            if ls.choice_active() then
-                ls.change_choice(1)
-            end
-        end, { silent = true })
-    end,
-    dependencies = {
-        { 'saadparwaiz1/cmp_luasnip' }
+            vim.keymap.set({ "i", "s" }, "<C-E>", function()
+                if ls.choice_active() then
+                    ls.change_choice(1)
+                end
+            end, { silent = true })
+        end,
+        dependencies = {
+            { 'saadparwaiz1/cmp_luasnip' }
+        }
+    },
+    {
+        "benfowler/telescope-luasnip.nvim",
+        config = function()
+            require("telescope").load_extension("luasnip")
+        end,
+        dependencies = {
+            { "nvim-telescope/telescope.nvim" },
+            { "L3MON4D3/LuaSnip" }
+        }
     }
 }
